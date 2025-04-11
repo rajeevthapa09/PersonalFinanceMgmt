16e1c364e638595951a4294f2bb4c2c6be16fbd5
rajeevthapa09 <rajeevthapa09@gmail.com>
2025-04-11 12:25:30 -0400

    building Post login home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,12 @@
 export default function Home(){
     return(
         <div>
+            <p>Welcome! </p>
+            <p>Here is your financial summary</p>
+            <p>Income:</p>
+            <p>Expense:</p>
+            <p>Budget Left:</p>
+
             
         </div>
     )
