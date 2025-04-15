c519b11f0112ec644f8dab9157afeadbd95847e4
rajeevthapa09 <rajeevthapa09@gmail.com>
2025-04-14 20:44:33 -0400

    Worked on adding financial summary on home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,10 +3,10 @@ export default function Home(){
     return(
         <div>
             <p>Welcome! </p>
-            <p>Here is your financial summary</p>
-            <p>Income:</p>
-            <p>Expense:</p>
-            <p>Budget Left:</p>
+            <p>Here is your financial summary for April, 2025</p>
+            <p>Income: $15,000</p>
+            <p>Expense: $14,000</p>
+            <p>Budget Left: $1,000</p>
 
             
         </div>
