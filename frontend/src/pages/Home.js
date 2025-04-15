cc0a89e3fbc4cbba1d65ab1d226bac2daac84e1d
rajeevthapa09 <rajeevthapa09@gmail.com>
2025-04-15 17:48:41 -0400

    Port over initial AddBudget layout from previous version (WIP)

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,22 @@
+import { useNavigate } from "react-router-dom";
 
 export default function Home(){
+    let navigate = useNavigate();
+
+    const addBudget = () => {
+        navigate("/addBudget")
+    }
     return(
         <div>
-            <p>Welcome! </p>
+            <p>Welcome, Rajeev! </p>
             <p>Here is your financial summary for April, 2025</p>
             <p>Income: $15,000</p>
             <p>Expense: $14,000</p>
             <p>Budget Left: $1,000</p>
+            <button>Add Expense</button>
+            <button>Add Income</button>
+            <button onClick={addBudget}>Add Budget</button>
+            <button>Logout</button>
 
             
         </div>
--- frontend/src/routes/UserDashboardLayout.js
+++ frontend/src/routes/UserDashboardLayout.js
@@ -1,10 +1,15 @@
 import {createBrowserRouter} from "react-router-dom";
 import Home from "../pages/Home"
+import AddBudget from "../pages/AddBudget";
 
 const appRouter = createBrowserRouter([
     {
         path: "/",
         element: <Home />
+    },
+    {
+        path:"/addBudget",
+        element: <AddBudget />
     }
     
 ])
