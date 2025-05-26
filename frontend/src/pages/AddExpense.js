fda39f3fa1c6fc5d913fadd48f8eae2d6a8cc7c1
rajeevthapa09 <rajeevthapa09@gmail.com>
2025-05-26 10:14:39 -0400

    WIP: working on developing addExpense.js page

--- frontend/src/pages/AddExpense.js
+++ frontend/src/pages/AddExpense.js
@@ -6,155 +6,41 @@ import { getBudget, storeBudget } from "../services/network";
 
 export default function AddExpense() {
 
-    const { globalState, setGlobalState } = useContext(GlobalContext);
-
-    const [expenseItems, setExpenseItems] = useState([{ category: "", estimated: "", actual: "", notes: "" }]);
-    const [errors, setError] = useState([]);
-    const [categories, setCategories] = useState(["Groceries", "Entertainment", "Gas", "Insurance"]);
-    const [customRow, setCustomRow] = useState([])
-
-    const refYear = useRef();
-    const refMonth = useRef();
-
-    let navigate = useNavigate();
-
-    const goBack = () => {
-        navigate("/")
-    }
-
-    const addNewExpenseRow = () => {
-        setExpenseItems((prev) => [...prev, { category: "", estimated: "", actual: "", notes: "" }])
-    }
-
-    const updateExpense = (rowIndex, field, value) => {
-        const copyExpense = [...expenseItems];
-        copyExpense[rowIndex][field] = value;
-        if (field === "category" && value === "Others" && !customRow.includes(rowIndex)) {
-            setCustomRow((prev) => ([...prev, rowIndex]));
-        }
-        setExpenseItems(copyExpense);
-    }
-
-
-    const viewBudget = async () => {
-        try {
-            const response = await getBudget(`${refYear.current.value}-${refMonth.current.value}`, globalState.userEmail)
-            if (response.data) {
-                const items = response.data.items;
-                const customIndexes = items.map((item, index) => !categories.includes(item.categories) ? index : null).filter(index => index !== null);
-                setExpenseItems(items);
-                setCustomRow(customIndexes); // restore custom field UI
-
-            } else {
-                setExpenseItems([{ category: "", estimated: "", actual: "", notes: "" }])
-                setCustomRow([]); // reset
-            }
-
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    useEffect(() => {
-        viewBudget();
-    }, [])
-
-    const handleSubmit = async () => {
-        let errorList = [];
-
-        for (let i = 0; i < expenseItems.length; i++) {
-            const errorRow = {};
-            if (expenseItems[i].category.trim() === "") {
-                errorRow.category = true;
-            }
-            if (expenseItems[i].estimated.trim() === "" || isNaN(expenseItems[i].estimated)) {
-                errorRow.estimated = true
-            }
-            errorList.push(errorRow);
-        }
-        setError(errorList);
-
-        let hasError = false;
-
-        for (let i = 0; i < errorList.length; i++) {
-            for (let key in errorList[i]) {
-                if (errorList[i][key]) {
-                    hasError = true;
-                    break;
-                }
-            }
-
-        }
-        if (hasError) {
-            alert("Please fix the highlighted fields before submitting.");
-            return;
-        }
-
-        const ret = await storeBudget({ items: expenseItems, date: `${refYear.current.value}-${refMonth.current.value}` }, globalState.userEmail);
-        if (ret.success) {
-            alert("successfully submitted");
-        }
-    }
-
-    return (
+      
+    return(
         <div className="page-container">
-            <button onClick={goBack}>Go back</button>
-            <label>Select Date: </label>
-            <select defaultValue={new Date().getFullYear()} ref={refYear} onChange={viewBudget}>
-                <option value="2025">2025</option>
-                <option value="2024">2024</option>
-                <option value="2023">2023</option>
-                <option value="2022">2022</option>
-                <option value="2021">2021</option>
+            <button>Go Back</button>
+            <p>Select Date: </p>
+            <select>
+                <option>2025</option>
+                <option>2024</option>
+                <option>2023</option>
+                <option>2022</option>
+                <option>2021</option>
             </select>
-            <select defaultValue={new Date().getMonth() + 1} ref={refMonth} onChange={viewBudget}>
-                <option value="1">January</option>
-                <option value="2">Febuary</option>
-                <option value="3">March</option>
-                <option value="4">April</option>
-                <option value="5">May</option>
-                <option value="6">June</option>
-                <option value="7">July</option>
-                <option value="8">August</option>
-                <option value="9">September</option>
-                <option value="10">October</option>
-                <option value="11">November</option>
-                <option value="12">December</option>
+            <select>
+                <option>January</option>
+                <option>Febuary</option>
+                <option>March</option>
+                <option>April</option>
+                <option>May</option>
+                <option>June</option>
+                <option>July</option>
+                <option>August</option>
+                <option>September</option>
+                <option>October</option>
+                <option>November</option>
+                <option>December</option>
             </select>
 
             <table>
-                <thead>
-                    <tr>
-                        <th>Category</th>
-                        <th>Estimated</th>
-                        <th>Actual</th>
-                        <th>Notes</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {expenseItems.map((row, index) => (
-                        <tr key={index}>
-                            <td>
-                                <select value={customRow.includes(index) ? "Others" : row.category} onChange={(e) => updateExpense(index, "category", e.target.value)} style={{ border: errors[index]?.category ? "2px solid red" : "2px solid #ccc" }}>
-                                    <option value="">Select categories</option>
-                                    {categories.map((cat, idx) =>
-                                        <option key={idx} value={cat}>{cat}</option>
-                                    )}
-                                    <option value="Others">Others</option>
-                                </select>
-                                {customRow.includes(index) && <input placeholder="Enter custom category" value={row.category === "Others" ? "" : row.category} type="text" onChange={(e) => { updateExpense(index, "category", e.target.value) }} style={{ marginTop: "4px", border: errors[index]?.category ? "2px solid red" : "2px solid #ccc" }} />}
-
-                            </td>
-                            <td><input value={row.estimated} type="text" onChange={(e) => updateExpense(index, "estimated", e.target.value)} /></td>
-                            <td><input value={row.actual} type="text" onChange={(e) => updateExpense(index, "actual", e.target.value)} /></td>
-                            <td><input value={row.notes} type="text" onChange={(e) => updateExpense(index, "notes", e.target.value)} style={{ border: "2px solid #ccc" }} /></td>
-                            <td>{index === expenseItems.length - 1 && (<button onClick={addNewExpenseRow}>+</button>)}</td>
-                        </tr>
-                    ))}
-                </tbody>
+                <tr>
+                    <th>Category</th>
+                    <th>Estimated</th>
+                    <th>Actual</th>
+                    <th>Notes</th>
+                </tr>
             </table>
-
-            <button onClick={handleSubmit}>Submit</button>
         </div>
     )
 }
