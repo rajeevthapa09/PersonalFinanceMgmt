68cc991ba4d88bceeaf489176fc118894b7314c7
rajeevthapa09 <rajeevthapa09@gmail.com>
2025-09-02 22:56:48 -0400

    Profile.js: added CSS to style the page

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,12 +1,21 @@
 
-export default function Profile(){
+import { useContext } from "react";
+import GlobalContext from "../context/GlobalContext";
+import Navbar from "../components/NavBar";
 
-    return(
-        <div>
-            Name: <input />
-            Login: <input />
-            Change Password: <input />
-            Image: <input />
+export default function Profile() {
+
+
+    const { globalState } = useContext(GlobalContext);
+    console.log ("gllo", globalState)
+    return (
+        <div className="page-container">
+            <Navbar />
+            <h2 className="page-title">Profile</h2>
+            Name: <input value={globalState.userName} /> <br />
+            Email: <input value={globalState.userEmail} /> <br />
+            Change Password: <input value="" /> <br />
+            Image: <input value="" /> <br />
         </div>
     )
 }
